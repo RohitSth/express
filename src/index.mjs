d45970df5761a1946a4f2acafe5d5fbc19a6b3ef
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-25 21:28:52 +0545

    learned get request

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (request, response) => {
+  response.status(201).send({ message: "Hello Me" });
+});
+
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, username: "user1", displayName: "U1" },
+    { id: 2, username: "user2", displayName: "U2" },
+    { id: 3, username: "user3", displayName: "U3" },
+  ]);
+});
+
+app.get("/api/products", (request, response) => {
+  response.send([
+    { id: 1, name: "product1", price: 100 },
+    { id: 2, name: "product2", price: 200 },
+    { id: 3, name: "product3", price: 300 },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
