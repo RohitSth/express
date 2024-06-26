4a4054c66941a027a573bafaedaf072af36f0686
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-26 21:17:22 +0545

    learned about route parameters

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,34 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const demoUsers = [
+  { id: 1, username: "user1", displayName: "U1" },
+  { id: 2, username: "user2", displayName: "U2" },
+  { id: 3, username: "user3", displayName: "U3" },
+];
+
 app.get("/", (request, response) => {
   response.status(201).send({ message: "Hello Me" });
 });
 
 app.get("/api/users", (request, response) => {
-  response.send([
-    { id: 1, username: "user1", displayName: "U1" },
-    { id: 2, username: "user2", displayName: "U2" },
-    { id: 3, username: "user3", displayName: "U3" },
-  ]);
+  response.send(demoUsers);
+});
+
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    return response.status(400).send({ message: "Invalid ID supplied" });
+  }
+
+  const findUser = demoUsers.find((user) => user.id === parsedId);
+  if (!findUser) {
+    return response.status(404).send({ message: "User not found" });
+  }
+  response.send(findUser);
+  // response.send(findUser.username);
 });
 
 app.get("/api/products", (request, response) => {
